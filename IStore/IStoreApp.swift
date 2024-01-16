be48d8f5cd9441af4ec33d1a98c5b8299efec3a5
Felix-Kariuki <felixmurimi280@gmail.com>
2024-01-16 22:11:04 +0300

    Initial Commit

---
+++ IStore/IStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  IStoreApp.swift
+//  IStore
+//
+//  Created by Felix kariuki on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct IStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
